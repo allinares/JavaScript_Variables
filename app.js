30592b86de6587cde41c6b654c039cbab63f1aa4
Alex <al@appalanche.io>
2024-05-01 09:13:53 -0400

    JavaScript Variables Exercise: Complete

--- app.js
+++ app.js
@@ -3,8 +3,38 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Alex";
+let lastName = "Lin";
+var age = 33;
 
-// Exercise 2
+lastName = "Smith";
+age = "48";
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first created in ${createdYear}.`);
+
+let product = "T-Shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(
+  `This product is a ${product}, with a low price of ${price}, in size ${selectedSize}.`
+);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hadRead = true;
+
+console.log(
+  `The book titled, ${title}, was written by 
+  ${author} and has a page count of ${pageCount}.`
+);
